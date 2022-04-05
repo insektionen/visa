6dec6c46562fa831d87ebad8ac37812b45fb33b9
Samuel Larsson <samuel@familjenlarsson.eu>
2022-04-05 21:44:30 +0200

    Changed endpoints to be more REST-like
    `/api/v0/legacy` -> `/api/v0/songs`
    `/api/v1/all` -> `/api/v1/songs`
    `/api/v1/song/{id}` -> `/api/v1/songs/{id}`

---
+++ pages/api/v1/songs/index.ts
@@ -0,0 +1,7 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getAllSongsData } from '../../../../util/songs';
+import { ResData, Song } from '../../../../util/types';
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse<ResData<Song[]>>) {
+	res.status(200).json(getAllSongsData());
+}
